52051d4daea6b7b5781571f7a23c99c1d242ce7d
Anjan Roy <hello@itzmeanjan.in>
2023-10-28 23:13:31 +0400

    pseudo random generation algorithm
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/rc4ok.rs
+++ src/rc4ok.rs
@@ -7,37 +7,79 @@ pub struct Rc4ok {
 
 impl Rc4ok {
     #[inline(always)]
-    pub fn init(key: &[u8], klen: usize) -> Self {
+    pub fn init(key: &[u8]) -> Self {
         let mut state = Self {
             s: [0u8; 256],
             i: 0,
             j: 0,
         };
-        Self::ksa(&mut state, key, klen);
+        state.ksa(key);
+
+        let mut dump = [0u8; 256];
+        state.prga(&mut dump);
+
         state
     }
 
     #[inline(always)]
-    fn ksa(state: &mut Self, key: &[u8], klen: usize) {
+    pub fn generate(&mut self, out: &mut [u8]) {
+        self.prga(out);
+    }
+
+    // Pseudo Random Generation Algorithm
+    #[inline(always)]
+    fn prga(&mut self, out: &mut [u8]) {
+        let mut idx = 0;
+        let olen = out.len();
+
+        while idx < olen {
+            self.i = self.i.wrapping_add(11);
+            self.j = self.j.rotate_left(1);
+
+            #[cfg(target_endian = "little")]
+            let mut j0 = self.j as u8;
+            #[cfg(target_endian = "big")]
+            let mut j0 = self.j.swap_bytes() as u8;
+
+            j0 = j0.wrapping_add(self.s[self.i as usize]);
+
+            self.s[self.i as usize] ^= self.s[j0 as usize];
+            self.s[j0 as usize] ^= self.s[self.i as usize];
+            self.s[self.i as usize] ^= self.s[j0 as usize];
+
+            let u = self.s[self.i as usize].wrapping_add(self.s[j0 as usize]);
+            out[idx] = self.s[u as usize];
+
+            idx += 1;
+        }
+    }
+
+    // Key Scheduling Algorithm
+    #[inline(always)]
+    fn ksa(&mut self, key: &[u8]) {
         let mut i = 0;
         let mut j = 0;
+        let klen = key.len();
 
         while i < 256 {
             j = (j + 233) % 256;
-            state.s[i] = j as u8;
+            self.s[i] = j as u8;
             i += 1;
         }
 
         j = 0;
         i = 0;
         while i < 256 {
-            j = (j + state.s[i] as usize + key[i % klen] as usize) % 256;
+            j = (j + self.s[i] as usize + key[i % klen] as usize) % 256;
 
-            state.s[i] ^= state.s[j];
-            state.s[j] ^= state.s[i];
-            state.s[i] ^= state.s[j];
+            self.s[i] ^= self.s[j];
+            self.s[j] ^= self.s[i];
+            self.s[i] ^= self.s[j];
 
             i += 1;
         }
+
+        self.i = self.s[j ^ 85];
+        self.j = 0;
     }
 }
