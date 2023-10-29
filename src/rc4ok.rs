9a94d07e1dee246c0c34be28979932bf56ec5807
Anjan Roy <hello@itzmeanjan.in>
2023-10-29 20:07:52 +0400

    add in code comments
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- src/rc4ok.rs
+++ src/rc4ok.rs
@@ -1,3 +1,7 @@
+/// RC4OK is a lightweight high-performance cryptographically strong random
+/// number generator based on an improved variant of RC4 stream cipher.
+///
+/// Note, classic RC4 is considered deprecated. The improvement RC4OK is proposed in https://ia.cr/2023/1486.
 #[derive(Copy, Clone)]
 pub struct RC4ok {
     s: [u8; 256],
@@ -6,26 +10,38 @@ pub struct RC4ok {
 }
 
 impl RC4ok {
+    /// Initializes the modified RC4 stream cipher state, given a non-empty secret
+    /// key as input. Once initialized the RC4OK stream cipher can be used for
+    /// generating arbitrary many pseudo random bytes.
     #[inline(always)]
     pub fn init(key: &[u8]) -> Self {
+        debug_assert!(key.len() > 0, "Key must be non-empty !");
+
         let mut state = Self {
             s: [0u8; 256],
-            i: 0,
             j: 0,
+            i: 0,
         };
         state.ksa(key);
 
+        // Ignore first 256 -bytes output of RC4OK psuedo random number generator.
         let mut dump = [0u8; 256];
         state.prga(&mut dump);
+        // This line can be optimized away, is it a "security" problem ?
+        dump.fill(0x00);
 
         state
     }
 
+    /// Generates `n` random bytes using the modified RC4 stream cipher,
+    /// following description on page 4 of https://ia.cr/2023/1486.
     #[inline(always)]
     pub fn generate(&mut self, out: &mut [u8]) {
         self.prga(out);
     }
 
+    /// Adds `16` -bit entropy into the internal state of the modified RC4 stream cipher,
+    /// following description on page 5 of https://ia.cr/2023/1486.
     #[inline(always)]
     pub fn add_entropy(&mut self, entropy: u16) {
         let mut jw1 = (self.j >> 16) as u16;
@@ -37,7 +53,43 @@ impl RC4ok {
         self.j = (self.j & MASK) | ((jw1 as u32) << 16);
     }
 
-    // Pseudo Random Generation Algorithm
+    /// Key Scheduling Algorithm
+    ///
+    /// Based on classic RC4 stream cipher's key scheduling algorithm.
+    /// This improved version of KSA is described on page 5 of https://ia.cr/2023/1486.
+    #[inline(always)]
+    fn ksa(&mut self, key: &[u8]) {
+        let klen = key.len();
+
+        let mut i = 0;
+        let mut j = 0;
+
+        while i < 256 {
+            j = (j + 233) % 256;
+            self.s[i] = j as u8;
+            i += 1;
+        }
+
+        j = 0;
+        i = 0;
+        while i < 256 {
+            j = (j + self.s[i] as usize + key[i % klen] as usize) % 256;
+
+            self.s[i] ^= self.s[j];
+            self.s[j] ^= self.s[i];
+            self.s[i] ^= self.s[j];
+
+            i += 1;
+        }
+
+        self.i = self.s[j ^ 85];
+        self.j = 0;
+    }
+
+    /// Pseudo Random Generation Algorithm
+    ///
+    /// Modified PRG algorithm, based on classic RC4 stream cipher's randomness generation
+    /// algorithm, desceibed on page 4 of https://ia.cr/2023/1486.
     #[inline(always)]
     fn prga(&mut self, out: &mut [u8]) {
         let mut idx = 0;
@@ -60,34 +112,5 @@ impl RC4ok {
 
             idx += 1;
         }
-    }
-
-    // Key Scheduling Algorithm
-    #[inline(always)]
-    fn ksa(&mut self, key: &[u8]) {
-        let mut i = 0;
-        let mut j = 0;
-        let klen = key.len();
-
-        while i < 256 {
-            j = (j + 233) % 256;
-            self.s[i] = j as u8;
-            i += 1;
-        }
-
-        j = 0;
-        i = 0;
-        while i < 256 {
-            j = (j + self.s[i] as usize + key[i % klen] as usize) % 256;
-
-            self.s[i] ^= self.s[j];
-            self.s[j] ^= self.s[i];
-            self.s[i] ^= self.s[j];
-
-            i += 1;
-        }
-
-        self.i = self.s[j ^ 85];
-        self.j = 0;
     }
 }
